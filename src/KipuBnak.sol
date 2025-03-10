eae62aa2441e22b3ff418a5587ff8b27dbc29b40
Vinicius Cisc <viwdni@gmail.com>
2025-03-10 18:09:24 -0300

    Initial commit and base contract

---
+++ src/KipuBnak.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+error KipuBank_InsufficientBalance();
+error KipuBank_ExceedsWithdrawLimit();
+error KipuBank_OverValueDeposit();
+error KipuBank_WithdrawValue();
+
+contract KipuBank {
+    // State variables
+
+    // Bank cap
+    uint256 public immutable s_BANK_CAP; 
+    
+    // Withdraw limit
+    uint256 public constant s_WITHDRAW_LIMIT = 100 ether;
+
+    // Mapping for user balances
+    mapping(address => uint256) public s_balances; // Key: _wallet (address), Value: _amount (uint256)
+
+    // Events
+    event Deposit(address indexed sender, uint256 amount);
+    event Withdraw(address indexed sender, uint256 amount);
+
+    // Constructor
+    constructor(uint256 bankCap) {
+        s_BANK_CAP = bankCap;
+    }
+
+    // Modifier to check if the deposit does not exceed the bank cap
+    modifier checkDepositLimit(uint256 depositAmount) {
+        uint256 newBalance = address(this).balance + depositAmount;
+        if (newBalance > s_BANK_CAP) {
+            revert KipuBank_OverValueDeposit();
+        }
+        _;
+    }
+
+    // Modifier to check if the user has enough balance to withdraw
+    modifier checkSufficientBalance(uint256 amount) {
+        if (s_balances[msg.sender] < amount) {
+            revert KipuBank_InsufficientBalance();
+        }
+        _;
+    }
+
+    // Modifier to check if the withdrawal does not exceed the limit
+    modifier checkWithdrawLimit(uint256 amount) {
+        if (amount > s_WITHDRAW_LIMIT) {
+            revert KipuBank_ExceedsWithdrawLimit();
+        }
+        _;
+    }
+    
+    //local correto do EMIT?
+    // Internal function to transfer ETH to the user
+    function _transferETH(address recipient, uint256 amount) internal {
+        (bool success, ) = recipient.call{value: amount}("");
+        
+        if (!success) {
+            revert KipuBank_WithdrawValue();
+        }
+    }
+
+    // Deposit function
+    function deposit() external payable checkDepositLimit(msg.value) {
+        
+        // Update user balance (_wallet: msg.sender, _amount: msg.value)
+        s_balances[msg.sender] += msg.value;
+
+        // Emit deposit event
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    // Withdraw function
+    function withdraw(uint256 amount) external 
+         checkWithdrawLimit(amount)
+        checkSufficientBalance(amount)
+    {
+        // Update balance before transfer (_wallet: msg.sender, _amount: amount)
+        s_balances[msg.sender] -= amount;
+
+        // Call internal transfer function to send ETH to user
+        _transferETH(msg.sender, amount);
+
+        // Emit withdrawal event
+        emit Withdraw(msg.sender, amount);
+    }
+
+    // Function  contract's ETH balance
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+}
