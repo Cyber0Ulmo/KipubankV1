47989f31b39112156e08030d6ef98b18637b555f
Vinicius Cisc <viwdni@gmail.com>
2025-03-10 20:28:08 -0300

    fix immutable s_bankCap (lowerCamelCase) modified error's name

--- src/KipuBnak.sol
+++ src/KipuBnak.sol
@@ -3,20 +3,21 @@ pragma solidity ^0.8.20;
 
 error KipuBank_InsufficientBalance();
 error KipuBank_ExceedsWithdrawLimit();
-error KipuBank_OverValueDeposit();
+error KipuBank_DepositExceedsCap();
 error KipuBank_WithdrawValue();
 
 contract KipuBank {
     // State variables
 
     // Bank cap
-    uint256 public immutable s_BANK_CAP; 
+    uint256 public immutable s_bankCap; 
     
     // Withdraw limit
     uint256 public constant s_WITHDRAW_LIMIT = 100 ether;
 
     // Mapping for user balances
-    mapping(address => uint256) public s_balances; // Key: _wallet (address), Value: _amount (uint256)
+    // Key: _wallet (address), Value: _amount (uint256)
+    mapping(address => uint256) public s_balances; 
 
     // Events
     event Deposit(address indexed sender, uint256 amount);
@@ -24,14 +25,13 @@ contract KipuBank {
 
     // Constructor
     constructor(uint256 bankCap) {
-        s_BANK_CAP = bankCap;
+        s_bankCap = bankCap;
     }
 
     // Modifier to check if the deposit does not exceed the bank cap
     modifier checkDepositLimit(uint256 depositAmount) {
-        uint256 newBalance = address(this).balance + depositAmount;
-        if (newBalance > s_BANK_CAP) {
-            revert KipuBank_OverValueDeposit();
+        if (address(this).balance > s_bankCap) {
+            revert KipuBank_DepositExceedsCap();
         }
         _;
     }
@@ -52,7 +52,7 @@ contract KipuBank {
         _;
     }
     
-    //local correto do EMIT?
+   
     // Internal function to transfer ETH to the user
     function _transferETH(address recipient, uint256 amount) internal {
         (bool success, ) = recipient.call{value: amount}("");
